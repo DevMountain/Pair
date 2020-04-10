7f728d62f1de7833ecd8fa6e7f08587a1a0c7150
Hin Wong <hinsolo@Hins-MacBook-Pro.local>
2020-04-10 11:24:35 -0600

    List TVC complete

--- PairGenrator/PairGenrator/ViewController/PersonDetailViewController.swift
+++ PairGenrator/PairGenrator/ViewController/PersonDetailViewController.swift
@@ -9,11 +9,16 @@
 import UIKit
 
 class PersonDetailViewController: UIViewController {
+    
+    //MARK: - Properties
+    var person: Person?
+    
+    //MARK: - Outlets
+    @IBOutlet weak var nameTextField: UITextField!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
     
 
