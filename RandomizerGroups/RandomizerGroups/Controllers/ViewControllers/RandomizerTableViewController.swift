fbc6b163db2d24cf2830376d3afa79d3fcec2c89
Kyle Warren <basedinbham@gmail.com>
2021-08-20 09:38:33 -0700

    Detail view, & delete added

--- RandomizerGroups/RandomizerGroups/Controllers/ViewControllers/RandomizerTableViewController.swift
+++ RandomizerGroups/RandomizerGroups/Controllers/ViewControllers/RandomizerTableViewController.swift
@@ -39,29 +39,14 @@ class RandomizerTableViewController: UITableViewController {
     // Override to support editing the table view.
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
-            // Delete the row from the data source
+            let personToDelete = RandomizerController.shared.people[indexPath.row]
+            RandomizerController.shared.deletePerson(person: personToDelete)
+            
             tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
+            
+        }
     }
-    
-    
-    /*
-     // Override to support rearranging the table view.
-     override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-     
-     }
-     */
-    
-    /*
-     // Override to support conditional rearranging of the table view.
-     override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-     // Return false if you do not want the item to be re-orderable.
-     return true
-     }
-     */
-    
+
     /*
      // MARK: - Navigation
      
