7d97be99973aa4bfba6ba39fbc04d6980a6f8c4c
Kyle Warren <basedinbham@gmail.com>
2021-08-20 08:52:12 -0700

    CoreData partially finished

---
+++ RandomizerGroups/RandomizerGroups/Controllers/ModelController/RandomizerController.swift
@@ -0,0 +1,19 @@
+//
+//  RandomizerController.swift
+//  RandomizerGroups
+//
+//  Created by Kyle Warren on 8/20/21.
+//
+
+import CoreData
+
+class RandomizerGroupsController {
+    
+    static let shared = RandomizerGroupsController()
+    
+    var people: [Person] = []
+    
+    
+    
+    
+} // End of Class
