80ee364d5d3c7f38ac0992b9d0ab83ce1edcb9bc
Kyle Warren <basedinbham@gmail.com>
2021-08-20 09:17:24 -0700

    CoreData done

--- RandomizerGroups/RandomizerGroups/Controllers/ModelController/RandomizerController.swift
+++ RandomizerGroups/RandomizerGroups/Controllers/ModelController/RandomizerController.swift
@@ -7,13 +7,38 @@
 
 import CoreData
 
-class RandomizerGroupsController {
+class RandomizerController {
     
-    static let shared = RandomizerGroupsController()
+    static let shared = RandomizerController()
     
     var people: [Person] = []
     
+    //MARK: - CRUD
     
+    func createPersonWith(name: String){
+        let newPerson = Person(name: name)
+        people.append(newPerson)
+        
+        CoreDataStack.saveContext()
+    }
     
+    func update(person: Person, name: String) {
+        person.name = name
+        
+        CoreDataStack.saveContext()
+    }
     
+    func toggleIsGrouped(person: Person) {
+        person.isGrouped.toggle()
+        
+        CoreDataStack.saveContext()
+    }
+    
+    func deletePerson(person: Person) {
+        guard let index = people.firstIndex(of: person) else { return }
+        people.remove(at: index)
+        
+        CoreDataStack.context.delete(person)
+        CoreDataStack.saveContext()
+    }
 } // End of Class
---
+++ RandomizerGroups/RandomizerGroups/Controllers/ViewControllers/RandomizerTableViewController.swift
@@ -0,0 +1,75 @@
+//
+//  RandomizerTableViewController.swift
+//  RandomizerGroups
+//
+//  Created by Kyle Warren on 8/20/21.
+//
+
+import UIKit
+
+class RandomizerTableViewController: UITableViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(true)
+        tableView.reloadData()
+    }
+    
+    // MARK: - Table view data source
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return RandomizerController.shared.people.count
+    }
+    
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "personCell", for: indexPath)
+        
+        let person = RandomizerController.shared.people[indexPath.row]
+        cell.textLabel?.text = person.name
+        
+        return cell
+    }
+    
+    
+    // Override to support editing the table view.
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            // Delete the row from the data source
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        } else if editingStyle == .insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    
+    
+    /*
+     // Override to support rearranging the table view.
+     override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+     
+     }
+     */
+    
+    /*
+     // Override to support conditional rearranging of the table view.
+     override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+     // Return false if you do not want the item to be re-orderable.
+     return true
+     }
+     */
+    
+    /*
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destination.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
+}
